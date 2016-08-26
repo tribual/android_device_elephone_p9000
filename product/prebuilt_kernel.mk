85ac453662a7edea41b4a4c00070d2fc9067c05f
Alex Kalles <alexakis1997@gmail.com>
2016-08-26 14:21:18 +0300

    Update prebuilt_kernel.mk

--- product/prebuilt_kernel.mk
+++ product/prebuilt_kernel.mk
@@ -1,10 +1,3 @@
-# Prebuilt Kernel
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilts/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
 # Prebuilt kernel
 PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+    $(LOCAL_PATH)/prebuilts/kernel:kernel
