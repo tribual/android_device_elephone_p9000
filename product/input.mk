ea44625dff6ed10074ae721123e258eb80e2cca5
leskal <alexakis1997@gmail.com>
2017-08-27 01:44:05 +0300

    Add Keymaster HAL interfaces package
    Change-Id: I433f58283b689c08a091bbc3842835bcb8b7bd71

--- product/input.mk
+++ product/input.mk
@@ -13,6 +13,11 @@ PRODUCT_SYSTEM_SERVER_JARS += com.cyanogenmod.keyhandler
 # Never dexopt the keyhandler
 $(call add-product-dex-preopt-module-config,com.cyanogenmod.keyhandler,disable)
 
+
 # Extra button package
 PRODUCT_PACKAGES += \
     EleButtonSettings
+
+# Keymaster HIDL interfaces
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0-impl
