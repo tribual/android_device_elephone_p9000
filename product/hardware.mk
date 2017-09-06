45d225def334bdbd5d11ea08ccd6ddc23bf05cf8
leskal <alexakis1997@gmail.com>
2017-09-06 19:49:41 +0300

    Add Doze Package and enable Ambient Display
    Change-Id: I674d4064398bca5bcc1c9774b2ce986c00f8fd5c

--- product/hardware.mk
+++ product/hardware.mk
@@ -16,7 +16,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
-
 PRODUCT_PACKAGES += \
    libmtk_symbols \
    libstlport
+
+# Doze Package
+PRODUCT_PACKAGES += \
+   Doze
